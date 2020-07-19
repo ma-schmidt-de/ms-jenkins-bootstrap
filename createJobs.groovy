4d1c5cbdfd6312dce969043b426e4563ceef90a8
ma.schmidt.de <matthias.schmidt@netcentric.biz>
2020-07-19 20:29:34 +0200

    Adding studio service pipeline

--- createJobs.groovy
+++ createJobs.groovy
@@ -6,3 +6,18 @@ pipelineJob('pipelineJobTemplate') {
         }
     }
 }
+
+pipelineJob('studio-service-job') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/ma-schmidt-de/spring-studios-service.git'
+                    }
+                    branch 'master'
+                }
+            }
+        }
+    }
+}
